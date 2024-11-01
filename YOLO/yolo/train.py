9d9c9a2f8014bfbf30c101dd693f420470ac97a5
batwan01 <wldhks1120@naver.com>
2024-11-01 19:42:51 +0900

    remove : YOLO

--- YOLO/yolo/train.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("ultralytics/cfg/models/v10/yolov10x.yaml")  # build a new model from YAML
-model = YOLO("yolov10x.pt")  # load a pretrained model (recommended for training)
-model = YOLO("ultralytics/cfg/models/v10/yolov10x.yaml").load("yolov10x.pt")  # build from YAML and transfer weights
-
-# Train the model
-results = model.train(data="/data/ephemeral/home/jiwan/2024-Autonomous-Driving-Artificial-Intelligence-Challenge/YOLO/yolo/data.yaml", epochs=20, imgsz=1024, batch=12)
